a55dda41edcde867ac4366f0a8bfd46c77410a87
Drako0013 <blackdrako013@hotmail.com>
2016-10-13 15:32:22 -0500

    fix menor

--- Validation_Utility.php
+++ Validation_Utility.php
@@ -45,7 +45,14 @@ class ValidationUtility{
 		return (ctype_alpha($char) or $char == ' ' or ValidationUtility::isSpecialSpanishCharacter($char));
 	}
 
-	public static function isAlphaAndSpacesString($string){
+	public static function isAlphaAndSpacesString(&$string){
+		$unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                            'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                            'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                            'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+		$string = strtr( $string, $unwanted_array );
+
 		$string_length = strlen($string);
 		for($i = 0; $i < $string_length; $i++){
 			if( !ValidationUtility::isAplhaOrSpace($string[$i]) ){
