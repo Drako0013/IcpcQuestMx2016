772c6b3e3a6788e221bf40d461c65baa55838ef3
Ethan Jimenez <ethanjimenez@localhost.localdomain>
2016-10-13 16:57:00 -0500

    Problemas con UTF-8 en validación

--- IcpcQuest.sql
+++ IcpcQuest.sql
@@ -27,7 +27,7 @@ CREATE TABLE ContestantChallengeCompletion(
 	state SMALLINT NOT NULL DEFAULT 0
 );
 
-ALTER DATABASE IcpcQuest CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE Contestant CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE Challenge CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-ALTER TABLE ContestantChallengeCompletion CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER DATABASE IcpcQuest CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE Contestant CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE Challenge CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE ContestantChallengeCompletion CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
